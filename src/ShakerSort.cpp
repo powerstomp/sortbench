fc7216401b2d01fb5a456dd62f4611cf38e24d41
HatsuneKareshi <m1e2a3n4n5@gmail.com>
2024-07-01 20:32:58 +0700

    Revert "added boolean variable to make shaker sort stop when no swap happened"
    This reverts commit 921b305f13b8973a0f87ddc57fab1807b9be2c02.

--- src/ShakerSort.cpp
+++ src/ShakerSort.cpp
@@ -3,28 +3,18 @@
 #include <utility>
 
 void Sort::Shaker::Apply(int* a, int n) {
-	bool swap = false;
 	int head = 0, tail = n - 1, k = 0;
 	while (head < tail) {
-		swap = false;
-
 		for (int i = head; i < tail; i++) {
 			if (a[i] > a[i + 1]) {
 				std::swap(a[i + 1], a[i]);
-				swap = true;
 				k = i;
 			}
 		}
 		tail = k;
-
-		if(!swap)
-			break;
-		swap = false;
-
 		for (int i = tail; i > head; i--) {
 			if (a[i] < a[i - 1]) {
 				std::swap(a[i - 1], a[i]);
-				swap = true;
 				k = i;
 			}
 		}
@@ -34,28 +24,18 @@ void Sort::Shaker::Apply(int* a, int n) {
 
 long long Sort::Shaker::CountComparisons(int* a, int n) {
 	long long res = 0;
-	bool swap = false;
 	int head = 0, tail = n - 1, k = 0;
 	while (++res && head < tail) {
-		swap = false;
-
 		for (int i = head; ++res && i < tail; i++) {
 			if (++res && a[i] > a[i + 1]) {
 				std::swap(a[i + 1], a[i]);
-				swap = true;
 				k = i;
 			}
 		}
 		tail = k;
-
-		if(++res && !swap)
-			break;
-		swap = false;
-
 		for (int i = tail; ++res && i > head; i--) {
 			if (++res && a[i] < a[i - 1]) {
 				std::swap(a[i - 1], a[i]);
-				swap = true;
 				k = i;
 			}
 		}
